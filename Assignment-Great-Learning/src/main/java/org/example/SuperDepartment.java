3e7980b609763aae83e6c62cd79af30f14f09ed3
pragyav180 <104020454+pragyav180@users.noreply.github.com>
2023-06-19 18:57:53 +0530

    Add files via upload

---
+++ Assignment-Great-Learning/src/main/java/org/example/SuperDepartment.java
@@ -0,0 +1,19 @@
+package org.example;
+public class SuperDepartment {
+
+    public String departmentName(){
+
+        return "Super Department";
+    }
+    public String  getTodaysWork(){
+        return "No Work as of now";
+
+    }
+    public String  getWorkDeadline(){
+        return null;
+    }
+    public String  isTodayAHoliday(){
+       return  "Today is not a holiday";
+    }
+
+}
